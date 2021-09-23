f10aec4d216127fcae590ae0602412dd9c903867
HongBeen <redbean-2@naver.com>
2021-09-24 02:40:04 +0900

    Navigation, about 스타일링

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
-import axios from "axios";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
-	return <div></div>;
+	return (
+		<HashRouter>
+			<Navigation />
+			<Route path="/" exact={true} component={Home} />
+			<Route path="/about" component={About} />
+		</HashRouter>
+	);
 }
+
+export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+	return (
+		<div class="about">
+			I love classic movies ❤️ <br />
+			Find your movie type!
+		</div>
+	);
+}
+
+export default About;
